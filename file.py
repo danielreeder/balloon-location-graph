17a3856f263959a1ae842a070d4715e3b7e1053e
danielreeder <101375530+danielreeder@users.noreply.github.com>
2023-06-03 11:48:09 -0700

    plotting and such

--- file.py
+++ file.py
@@ -1,3 +1,31 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
 
-print(pd.read_csv('KK7MXV-11.csv', usecols=["latitude", "longitude"]).head())
+def get_hour(time):
+    return time.hour
+
+def get_time(time):
+    return (time.hour*60 + time.minute) / 60
+
+print(pd.read_csv('KK7MXV-11.csv', usecols=["lat", "lng"]).head())
+print(pd.read_csv('data_bme280.csv').head())
+print(pd.read_csv('data_pms5003.csv').head())
+bme = pd.read_csv('data_bme280.csv')
+pms = pd.read_csv('data_pms5003.csv')
+bme['Time'] = pd.to_datetime(bme['Time'], format='mixed').apply(datetime.time).apply(get_time)
+pms['Time'] = pd.to_datetime(pms['Time'], format='mixed').apply(datetime.time).apply(get_time)
+print(bme['Time'])
+print(pms.columns)
+bme.plot.scatter(x='Time', y=' humidity')
+bme.plot.scatter(x='Time', y=' pressure')
+bme.plot.scatter(x='Time', y=' temperature')
+pms.plot.scatter(x='Time', y=[' < 0.3 PM'])
+pms.plot.scatter(x='Time', y=[' <0.5 PM'])
+pms.plot.scatter(x='Time', y=[' <1.0 PM'])
+pms.plot.scatter(x='Time', y=[' <2.5 PM'])
+pms.plot.scatter(x='Time', y=[' <10 PM'])
+
+plt.xlabel("Time (hours from midnight)")
+plt.show()
--- plot.py
+++ plot.py
@@ -50,12 +50,14 @@ class MainWindow(QMainWindow):
         self.layout_temp = QVBoxLayout()
         
         self.sc = MplCanvas(self, width=50, height=30, dpi=100)
-        # self.sc_temp = MplCanvas(self, width=50, height=30, dpi=100)
+        self.sc_temp = MplCanvas(self, width=50, height=30, dpi=100)
         df_kk7 = pd.read_csv('KK7MXV-11.csv')
         df_ko4 = pd.read_csv('KO4TL-11.csv')
-        # df_kk7['temp'] = df_kk7[['comment']].apply(self.parse_comment, axis=1)
-        # df_ko4['temp'] = df_ko4[['comment']].apply(self.parse_comment, axis=1)
-        # print("ko4:" + df_ko4['temp'])
+        print(df_ko4)
+        df_kk7['temp'] = df_kk7[['comment']].apply(self.parse_comment, axis=1).apply(str.strip, args='C')
+        df_ko4['temp'] = df_ko4[['comment']].apply(self.parse_comment, axis=1).apply(str.strip, args='C')
+        df_kk7['temp'][0] = 0
+        df_ko4['temp'][0] = 0
         states = gpd.read_file('data/usa-states-census-2014.shp')
         states.query("NAME == 'Oregon'").boundary.plot(color='black', ax=self.sc.ax)  
         roads = gpd.read_file('data/USA_roads.shp')
@@ -68,34 +70,37 @@ class MainWindow(QMainWindow):
         self.sc.ax.set_xlim(min(min(df_kk7['lng']), min(df_ko4['lng']))-.05, max(max(df_kk7['lng']), max(df_ko4['lng']))+.05)
         self.sc.ax.set_ylim(min(min(df_kk7['lat']), min(df_ko4['lat']))-.05, max(max(df_kk7['lat']), max(df_ko4['lat']))+.05)
         # self.sc_temp.ax.scatter(x=df_kk7['lng'], y=df_kk7['lat'], c=df_kk7['temp'], cmap='plasma', s=10)
-        # self.sc_temp.ax.scatter(x=df_ko4['lng'], y=df_ko4['lat'], c=df_ko4['temp'], cmap='plasma', s=10)
-        # self.sc_temp.ax.grid(alpha=0)
-        # self.sc_temp.ax.set_xlabel('Longitude')
-        # self.sc_temp.ax.set_ylabel('Latitude')
-        # self.sc_temp.ax.set_xlim(min(min(df_kk7['lng']), min(df_ko4['lng']))-.05, max(max(df_kk7['lng']), max(df_ko4['lng']))+.05)
-        # self.sc_temp.ax.set_ylim(min(min(df_kk7['lat']), min(df_ko4['lat']))-.05, max(max(df_kk7['lat']), max(df_ko4['lat']))+.05)
+        self.sc_temp.ax.scatter(x=df_ko4['lng'], y=df_ko4['lat'], c=df_ko4['temp'], cmap='plasma', s=10)
+        self.sc_temp.ax.grid(alpha=0)
+        self.sc_temp.ax.set_xlabel('Longitude')
+        self.sc_temp.ax.set_ylabel('Latitude')
+        self.sc_temp.ax.set_xlim(min(min(df_kk7['lng']), min(df_ko4['lng']))-.05, max(max(df_kk7['lng']), max(df_ko4['lng']))+.05)
+        self.sc_temp.ax.set_ylim(min(min(df_kk7['lat']), min(df_ko4['lat']))-.05, max(max(df_kk7['lat']), max(df_ko4['lat']))+.05)
         submit = QPushButton('Swap')
         submit.clicked.connect(self.update_plot)
         self.j = 0
         
         self.signal.connect(self.update_plot)
         self.layout.addWidget(submit)
-        self.layout.addWidget(self.sc)
-        # self.layout_temp.addWidget(self.c.sc_temp)
+        # self.layout.addWidget(self.sc)
+        self.layout_temp.addWidget(self.sc_temp)
         
-        self.widget.setLayout(self.layout)
+        # self.widget.setLayout(self.layout)
+        self.widget.setLayout(self.layout_temp)
         self.setCentralWidget(self.widget)
         self.show()
         
     def update_plot(self):
         if j%2 == 0:
             self.widget.setLayout(self.layout_temp)
-            j += 1
+            self.setCentralWidget(self.widget)
+            self.show()
+            self.j += 1
             return
         self.widget.setLayout(self.layout)
+        self.j += 1
 
     def parse_comment(self, comment):
-        print(comment[0])
         return comment[0].split(' ')[1]
 
 def main():
